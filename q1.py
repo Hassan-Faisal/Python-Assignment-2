023fa22e3f5c448cf7a04bf12639ecd29c0f4bc7
Hassan Faisal <iamhassanfaisal@gmail.com>
2024-08-05 13:57:52 +0500

    save

--- q1.py
+++ q1.py
@@ -1,6 +1,5 @@
 
-num_1 : str = float(input ("Enter first number : "))
-# convert : int = int(num_1)
-num_2 : str = float(input ("Enter second number : "))
+num_1 : int = int(input ("Enter first number : "))
+num_2 : int = int(input ("Enter second number : "))
 sum : float = num_1 + num_2
 print(f"Sum of the above two numbers is : {sum}")
